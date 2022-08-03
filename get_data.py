2c169f7b80a6c339b83bde13216b3057a4ad5fdd
msrlab <martin@schulte-ruether.de>
2022-08-03 20:40:41 +0200

    <feat><fix> adds some features and some fixes -adds an info logger and a debugging logger -adds support for automatic download of available subject folders -some explicit catches for common download errors (missing directories etc.) - support for sequential handling of several download settings

--- get_data.py
+++ get_data.py
@@ -1,9 +1,13 @@
 import pysftp
 import os
 import traceback
+import logging
+from datetime import datetime
 
 from secret import login_credentials # secret login info in separate file
-from settings import local_settings # local settings info in separate file
+#from settings import local_settings # local settings info in separate file
+
+from settings import *
 # alternatively: enter here
 # class login_credentials():
 #     def __init__(self):
@@ -11,97 +15,182 @@ from settings import local_settings # local settings info in separate file
 #         self.user = "myusername"
 #         self.pswd = "mypassword"
 
+##### functions
+def setup_logger(name, log_file, level):
+    """To setup as many loggers as you want"""
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    if level >= logging.INFO:
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
+
 def make_dir(local_path):
     if not os.path.exists(local_path):
         os.makedirs(local_path)
-        print("directory created: " +local_path)
+        log_it("directory created: " +local_path, logging.INFO)
+
+def log_it(msg,level):
+    for lg in logger:
+        lg.log(level,msg)
+
+now_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+#logging.basicConfig(filename='get_data'+now_str+'.log', level=logging.WARNING)
+#logging.basicConfig(filename='get_data'+now_str+'.log', level=logging.WARNING)
+
+# construct to separate loggers for debugging and warning
+logger_info = [{"name":'info_logger',"level":logging.INFO},
+          {"name":'debug_logger',"level":logging.DEBUG}]
+logger=[]
+for li in logger_info:
+    logger.append(setup_logger(li["name"], li["name"]+'_get_data_'+now_str+'.log', li["level"]))
+
+# info_logger = setup_logger('info_logger', 'info_get_data_'+now_str+'.log', logging.INFO)
+# info_logger.info("###logfile for download process (info and warnings)")
+# # detailed debug log
+# debug_logger = setup_logger('debug_logger', 'debug_get_data_'+now_str+'.log', logging.DEBUG)
+# debug_logger.info("###logfile for download process (detailled debugging log)")
 
 lc = login_credentials()
 ls = local_settings()
 make_dir(ls.local_path)
 
 with pysftp.Connection(host=lc.host, username=lc.user, password=lc.pswd) as sftp:
-    print("Connection successfully established ... ")
-    for td in ls.time_dirs:
-        if ls.mode == "overall":
-            print("downloading complete folders (one folder containing data of many subjects)")
-            try:
-                base_remote_dir = os.path.join(remote_path, td)
-                sftp.chdir(base_remote_dir)
-                local_dir = os.path.join(ls.local_path, td)
-                make_dir(local_dir)
-                for o_dir in ls.overall_dirs:
-                    print("remote dir:" + os.path.join(base_remote_dir, o_dir))
-                    print("local dir:" + os.path.join(local_dir, o_dir))
+    log_it("Connection to " + lc.host + " successfully established ... ", logging.INFO)
+    for dl_i, dl in enumerate(dl_tasks):
+        try:
+            log_it("working on: download setting " + str(dl_i+1) +"/" + str(len(dl_tasks))+" : " + dl.description, logging.INFO)
+            for td in dl.time_dirs:
+                if dl.mode == "overall":
+                    log_it("downloading complete folders (one folder containing data of many subjects)", logging.INFO)
                     try:
-                        sftp.get_r(o_dir, local_dir)
-                        print("overall folder download successful!")
-                    except:
-                        print("overall folder download not successful!")
-                        traceback.print_exc(limit=1)
-
-            except:
-                print("problem with directory " + base_remote_dir)
-                traceback.print_exc(limit=1)
-        else:
-            try:
-                base_remote_dir = os.path.join(ls.remote_path, td, ls.intermed_dir1)
-                sftp.chdir(base_remote_dir)
-                for subj in ls.subjs:
-                    if ls.mode == "subjects":
-                        local_dir = os.path.join(ls.local_path, td, ls.intermed_dir1)
+                        base_remote_dir = os.path.join(ls.remote_path, td)
+                        sftp.chdir(base_remote_dir)
+                        local_dir = os.path.join(ls.local_path, td)
                         make_dir(local_dir)
-                        print("remote dir:" + os.path.join(base_remote_dir, subj))
-                        print("local dir:" + os.path.join(local_dir,subj))
-                        try:
-                            sftp.get_r(subj, local_dir)
-                            print("recursive subject download successful!")
-                        except:
-                            print("subject download not successful!")
-                            traceback.print_exc(limit=1)
-                    else:
-                        try:
-                            base_remote_dir =  os.path.join(ls.remote_path, td, ls.intermed_dir1, subj)
+                        for o_dir in dl.overall_dirs:
+                            log_it("downloading folder " + os.path.join(base_remote_dir, o_dir) +
+                                   " --> " + os.path.join(local_dir, o_dir), logging.INFO)
+                            try:
+                                sftp.get_r(o_dir, local_dir)
+                                log_it("overall folder download successful!",logging.INFO)
+                            except:
+                                log_it("overall folder download not successful!", logging.WARN)
+                                exc_str = traceback.format_exc(limit=1)
+                                log_it(exc_str, logging.DEBUG)
+
+                    except:
+                        log_it("problem with directory " + base_remote_dir, logging.WARN)
+                        exc_str = traceback.format_exc(limit=1)
+                        log_it(exc_str, logging.DEBUG)
+                else:
+                    try:
+                        base_remote_dir = os.path.join(ls.remote_path, td, dl.intermed_dir1)
+                        if sftp.exists(base_remote_dir):
                             sftp.chdir(base_remote_dir)
-                            local_dir = os.path.join(ls.local_path, td, ls.intermed_dir1, subj)
-                            make_dir(local_dir)
-                            for dir2 in ls.dirs:
-                                if ls.mode == "dirs":
-                                    #l_path = os.path.join(ls.local_path, td, ls.intermed_dir1, subj, dir2)
-                                    #make_dir(l_path)
-                                    #
-                                    #local_dir = os.path.join(ls.local_path)
-                                    print("remote dir:" + os.path.join(base_remote_dir,dir2))
-                                    print("local dir:" + os.path.join(local_dir,dir2))
+
+
+
+
+                            if len(dl.subjs) == 0:
+                                log_it("getting available subjects from folder " + base_remote_dir, logging.INFO)
+                                subjs = sftp.listdir()
+                                log_it("downloading " + str(len(subjs)) + " subject directories from " + base_remote_dir, logging.INFO)
+                            else:
+                                subjs = dl.subjs
+                            for subj in subjs:
+                                if dl.mode == "subjects":
+                                    local_dir = os.path.join(ls.local_path, td, dl.intermed_dir1)
+                                    make_dir(local_dir)
+                                    log_it("downloading folder " + os.path.join(base_remote_dir, subj) +
+                                          " --> " + os.path.join(local_dir,subj), logging.INFO)
                                     try:
-                                        sftp.get_r(dir2, local_dir)
-                                        print("recursive folder download successful!")
+                                        if sftp.exists(subj) == True:
+                                            sftp.get_r(subj, local_dir)
+                                            log_it("recursive subject download successful! (subject " +subj + ")", logging.INFO)
+                                        else:
+                                            log_it(os.path.join(base_remote_dir,subj) + " does not exist on " + lc.host,
+                                                       logging.WARN)
                                     except:
-                                        print("folder download not successful!")
-                                        traceback.print_exc(limit=1)
-                                if ls.mode == "files":
-                                    for subj_file in ls.subj_files:
-                                        local_dir = os.path.join(ls.local_path, td, ls.intermed_dir1, subj, dir2)
+                                        log_it("subject download not successful! (subject " +subj + ")", logging.WARNING)
+                                        exc_str = traceback.format_exc(limit=1)
+                                        log_it(exc_str, logging.DEBUG)
+                                        #traceback.print_exc(limit=1)
+                                else:
+                                    try:
+                                        base_remote_dir =  os.path.join(ls.remote_path, td, dl.intermed_dir1, subj)
+                                        sftp.chdir(base_remote_dir)
+                                        local_dir = os.path.join(ls.local_path, td, dl.intermed_dir1, subj)
                                         make_dir(local_dir)
-                                        dl_dir = os.path.join(ls.remote_path, td, ls.intermed_dir1, subj, dir2)
-                                        l_path = os.path.join(ls.local_path, td, ls.intermed_dir1, subj, dir2)
-                                        dl_file = os.path.join(dl_dir, subj_file)
-                                        local_file = os.path.join(local_dir, subj_file)
-                                        try:
-                                            if ls.mode == "files":
-                                                print("remote file:" + dl_file)
-                                                print("local file:" + local_file)
-                                                sftp.get(dl_file, local_file)
-                                                print("file download successful!")
-                                        except:
-                                            print("Download not successful!")
-                                            traceback.print_exc(limit=1)
-                        except:
-                            print("problem with directory " + os.path.join(ls.remote_path, td, ls.intermed_dir1, subj))
-                            traceback.print_exc(limit=1)
-            except:
-                print("problem with directory " + os.path.join(ls.remote_path, td, ls.intermed_dir1))
-                traceback.print_exc(limit=1)
+                                        for dir2 in dl.dirs:
+                                            if dl.mode == "dirs":
+                                                #l_path = os.path.join(dl.local_path, td, dl.intermed_dir1, subj, dir2)
+                                                #make_dir(l_path)
+                                                #
+                                                #local_dir = os.path.join(dl.local_path)
+                                                log_it("downloading folder " + os.path.join(base_remote_dir,dir2) +
+                                                       " --> " + os.path.join(local_dir,dir2), logging.INFO)
+                                                try:
+                                                    if sftp.exists(dir2) == True:
+                                                        sftp.get_r(dir2, local_dir)
+                                                        log_it(
+                                                            "recursive subject download successful! (subject " + subj + ")",
+                                                            logging.INFO)
+                                                    else:
+                                                        log_it(os.path.join(base_remote_dir,
+                                                                            dir2) + " does not exist on " + lc.host,
+                                                               logging.WARN)
+                                                except:
+                                                    log_it("folder download not successful!", logging.WARN)
+                                                    exc_str = traceback.format_exc(limit=1)
+                                                    log_it(exc_str, logging.DEBUG)
+                                            if dl.mode == "files":
+                                                for subj_file in dl.subj_files:
+                                                    local_dir = os.path.join(ls.local_path, td, dl.intermed_dir1, subj, dir2)
+                                                    make_dir(local_dir)
+                                                    dl_dir = os.path.join(ls.remote_path, td, dl.intermed_dir1, subj, dir2)
+                                                    l_path = os.path.join(ls.local_path, td, dl.intermed_dir1, subj, dir2)
+                                                    dl_file = os.path.join(dl_dir, subj_file)
+                                                    local_file = os.path.join(local_dir, subj_file)
+                                                    try:
+                                                        if dl.mode == "files":
+                                                            log_it("downloading file " + dl_file +
+                                                                   " --> " + local_file, logging.INFO)
+                                                            #print("remote file:" + dl_file)
+                                                            #print("local file:" + local_file)
+                                                            sftp.get(dl_file, local_file)
+                                                            log_it("file download successful!", logging.INFO)
+                                                    except:
+                                                        log_it("Download not successful!",logging.WARN)
+                                                        exc_str = traceback.format_exc(limit=1)
+                                                        log_it(exc_str, logging.DEBUG)
+                                    except:
+                                        log_it("problem with directory " + os.path.join(ls.remote_path, td, dl.intermed_dir1, subj),logging.WARN)
+                                        exc_str = traceback.format_exc(limit=1)
+                                        log_it(exc_str, logging.DEBUG)
+
+                        else:
+                            log_it(base_remote_dir + " does not exist on " + lc.host,
+                                                   logging.WARN)
+                    except:
+                        log_it("problem with directory " + os.path.join(ls.remote_path, td, dl.intermed_dir1), logging.WARN)
+                        exc_str = traceback.format_exc(limit=1)
+                        log_it(exc_str, logging.DEBUG)
+        except:
+            log_it("CAUTION: download setting " + str(dl_i + 1) + "/" + str(len(dl_tasks)) + " : " + dl.description + "did not finish without errors!",
+                   logging.WARN)
 
 # connection closed automatically
-print("connection closed")
+if sftp._sftp_live == False:
+    log_it("connection closed", logging.INFO)
+
+#close logging handlers for files to become immediately available
+for lgr in logger:
+    handler = lgr.handlers[0]
+    lgr.removeHandler(handler)
+    handler.close()
--- settings_template.py
+++ settings_template.py
@@ -2,19 +2,82 @@ class local_settings():
     def __init__(self):
         self.remote_path = "/data/imagen/2.7" #the remote path on the sftp server
         self.local_path ="/my/local/datapath/" #your local path where the data will be downloaded
-        self.time_dirs = ["BL", "FU1"]
-        # time_dirs = ["BL","FU1","FU2","FU3"]
+        self.log_path ="/my/local/download_logs" #give a path where logs can be saved
 
-        # mode = "dirs"   # "files" or "dirs" or "subjects"
-        self.mode = "subjects"  # "files" or "dirs" or "subjects" or "overall"
-        # "overall": all directories, subdirectories and files within a folder remote_path/{time_dirs}/{overall_dirs}
-        self.overall_dirs = ["dawba/", "geolocation/", "cantab/", "meta_data/", "psytools/"]
-        # "dirs": n directories per subject are downloaded remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}
-        # "files": one file per subject is downloaded: remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}/{subj_files}
-        self.intermed_dir1 = "imaging/spm_first_level"
-        self.subjs = ["000099616225", "000085724167"]
-        self.dirs = ["EPI_stop_signal/", "EPI_short_MID/"]
+class dl_settings():
+    def __init__(self, description, mode, time_dirs, overall_dirs, intermed_dir1, subjs, dirs, subj_files):
+        self.description = description
+        # string, short description what the given settings will do
+        # e.g. "download available data from all timepoints and subjects for imaging/spm_first_level/.../EPI_short_MID/",
+        self.mode = mode  # string, "files" or "dirs" or "subjects" or "overall"
+        # the folder structure of the server is: remote_path/{time_dirs}/intermed_dirs/
+        # some of the intermed_dirs (e.g. imaging/spm*) are organised as follows: remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}
+        # the "mode" setting provides several ways of downloading parts of the data, using different hierarchies
+        # "overall": download all directories, subdirectories and files within the given folders  remote_path/{time_dirs}/{overall_dirs}
+        # "dirs": download n directories per subject within remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}
+        # "files": download n files from folder remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}/{subj_files}
+        #          dirs should have only one entry in this case, because files are expected to be present within each dir
+        # "subjects": dowload n subject folders from: remote_path/{time_dirs}/intermed_dir/
+        self.time_dirs = time_dirs
+        # list of strings, example:  ["BL","FU1","FU2","FU3"]
+        self.overall_dirs = overall_dirs
+        # list of strings, e.g. ["dawba/", "geolocation/", "cantab/", "meta_data/", "psytools/"]
+        self.intermed_dir1 = intermed_dir1
+        # string, e.g. "imaging/spm_first_level"
+        self.subjs = subjs
+        # list of strings, e.g. ["000099616225", "000085724167"]
+        # leave empty (self.subjs = [])to download all available subj-subdirectories within intermed_dir1
+        self.dirs = dirs
+        # list of strings, e.g. ["EPI_stop_signal/", "EPI_short_MID/"]
+        # should only have one element when using mode "files"
+        self.subj_files = subj_files
+        # list of strings, e.g. ["con_0006_stop_failure_-_stop_success.nii.gz",
+        #                        "con_0005_stop_success_-_stop_failure.nii.gz"]
         # if mode = "dir" subj_files is ignored
-        # if dir2 has more than 1 element and mode="files" subj_files are expected in each directory
-        self.subj_files = ["con_0006_stop_failure_-_stop_success.nii.gz",
-                      "con_0005_stop_success_-_stop_failure.nii.gz"]
+
+dl_fMRI_MID = dl_settings(
+    description="download available data from all timepoints and subjects for imaging/spm_first_level/.../EPI_short_MID/",
+    mode="dirs",  # string, "files" or "dirs" or "subjects" or "overall"
+    # the folder structure of the server is: remote_path/{time_dirs}/intermed_dirs/
+    # some of the intermed_dirs (e.g. imaging/spm*) are organised as follows: remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}
+    # the "mode" setting provides several ways of downloading parts of the data, using different hierarchies
+    # "overall": download all directories, subdirectories and files within the given folders  remote_path/{time_dirs}/{overall_dirs}
+    # "dirs": download n directories per subject within remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}
+    # "files": download n files from folder remote_path/{time_dirs}/intermed_dir/{subjs}/{dirs}/{subj_files}
+    #          dirs should have only one entry in this case, because files are expected to be present within each dir
+    # "subjects": dowload n subject folders from: remote_path/{time_dirs}/intermed_dir/
+    time_dirs=["BL", "FU1", "FU2", "FU3"],
+    # list of strings, example:  ["BL","FU1","FU2","FU3"]
+    overall_dirs=[],
+    # list of strings, e.g. ["dawba/", "geolocation/", "cantab/", "meta_data/", "psytools/"]
+    intermed_dir1="imaging/spm_first_level",
+    # string, e.g. "imaging/spm_first_level"
+    # subjs = [],
+    subjs=["000085724167"],
+    # list of strings, e.g. ["000099616225", "000085724167"]
+    # leave empty (self.subjs = [])to download all available subj-subdirectories within intermed_dir1
+    dirs=["EPI_short_MID/"],
+    # list of strings, e.g. ["EPI_stop_signal/", "EPI_short_MID/"]
+    # should only have one element when using mode "files"
+    subj_files=[]
+    # list of strings, e.g. ["con_0006_stop_failure_-_stop_success.nii.gz",
+    #                        "con_0005_stop_success_-_stop_failure.nii.gz"]
+    # if mode = "dir" subj_files is ignored
+)
+
+dl_overall_data = dl_settings(
+    description="download available data from overall folders (dawba,geolocation,cantab,meta_data,psytools)",
+    mode="overall",
+    time_dirs=["BL", "FU1", "FU2", "FU3"],
+    overall_dirs=["dawba/", "geolocation/", "cantab/", "meta_data/", "psytools/"],
+    intermed_dir1="",
+    subjs=[],
+    dirs=[],
+    subj_files=[]
+)
+
+# put all definitions here into a list, and they will be downloaded sequentially
+dl_tasks = [
+    dl_fMRI_MID,
+    dl_overall_data
+]
